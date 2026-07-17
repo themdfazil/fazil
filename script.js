c9f92798ea758b5b4ab4b6a0da0b47348ba6f14c
THE MD FAZIL <themdfazil.0000@gmail.com>
2026-07-17 21:59:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleBtn = document.getElementById('theme-toggle');
-    themeToggleBtn.addEventListener('click', () => {
-        document.body.classList.toggle('light-theme');
-    });
-});
